8cf749c1c10aff9a171ba445c2121c0bf67a3976
jamesbak@microsoft.com <jamesbak@microsoft.com>
2016-02-05 18:41:12 -0800

    Improved logging capture

--- hazelcast-on-ubuntu/initialize-hazelcast.sh
+++ hazelcast-on-ubuntu/initialize-hazelcast.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
-# Usage: bootstrap-hazelcast.sh {hz-userName} {hz-password} {hz-version} {[cluster-nodes-static-ip[,]]} {[partition-group:[node-static-ips[,]][;]]}
-# Sample: bootstrap-hazelcast.sh 'test-hz-jb' 'hazelcastpassword' '3.4.1' '10.0.0.4,10.0.0.5' '0:10.0.0.4,10.0.0.9;1:10.0.0.5,10.0.0.10'
-
-# hzlogfile="/var/log/hazelcast-bootstrap.log"
-# touch $hzlogfile 
-# chmod a+rw $hzlogfile
 execname=$0
 scriptstatus=$0
 LOG_DIR=/var/log/initialize-hazelcast
 LOG_DIR_FILE=/var/log/initialize-hazelcast/initialize-hazelcast.log
 LOG_DIR_SERVER=/var/log/initialize-hazelcast/initialize-hazelcast-server-sh.log
 mkdir $LOG_DIR
+
 log() {
   echo "[${execname}] $@" >> $LOG_DIR_FILE
 }
@@ -22,7 +17,7 @@ reliableaptget() {
  COMMAND_STATUS=1
  until [ $COMMAND_STATUS -eq 0 ] || [ $wait_time -eq $TIME_MAX ]; do
    echo "try $getcommand with timeincrement $wait_time"
-   $getcommand >> $LOG_DIR_FILE
+   $getcommand >> LOG_DIR_FILE 2>>&1
    COMMAND_STATUS=$?
    echo "command status $COMMAND_STATUS"
    wait_time=$(($wait_time + $TIME_INCREMENT))
@@ -83,16 +78,16 @@ if [ $scriptstatus -ne 0 ]
 fi
 
 # Need to download the Azure Discovery API using Maven & manually load the dependencies
-mvn dependency:copy -Dartifact=com.hazelcast.azure:hazelcast-azure:$2 -DoutputDirectory=/var/lib/hazelcast-$1/lib
-mvn dependency:copy -Dartifact=com.microsoft.azure:azure-mgmt-compute:0.9.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib
-mvn dependency:copy -Dartifact=com.microsoft.azure:azure-mgmt-resources:0.9.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib
-mvn dependency:copy -Dartifact=com.microsoft.azure:azure-mgmt-network:0.9.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib
-mvn dependency:copy -Dartifact=com.microsoft.azure:azure-mgmt-utility:0.9.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib
-mvn dependency:copy -Dartifact=com.microsoft.azure:azure-core:0.9.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib
-mvn dependency:copy -Dartifact=org.apache.httpcomponents:httpclient:4.5.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib
-mvn dependency:copy -Dartifact=com.microsoft.azure:adal4j:1.1.2 -DoutputDirectory=/var/lib/hazelcast-$1/lib
-mvn dependency:copy -Dartifact=com.nimbusds:oauth2-oidc-sdk:4.5 -DoutputDirectory=/var/lib/hazelcast-$1/lib
-mvn dependency:copy -Dartifact=com.google.code.gson:gson:2.2.4 -DoutputDirectory=/var/lib/hazelcast-$1/lib
+mvn dependency:copy -Dartifact=com.hazelcast.azure:hazelcast-azure:$2 -DoutputDirectory=/var/lib/hazelcast-$1/lib >> $LOG_DIR_FILE 2>>&1
+mvn dependency:copy -Dartifact=com.microsoft.azure:azure-mgmt-compute:0.9.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib >> $LOG_DIR_FILE 2>>&1
+mvn dependency:copy -Dartifact=com.microsoft.azure:azure-mgmt-resources:0.9.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib >> $LOG_DIR_FILE 2>>&1
+mvn dependency:copy -Dartifact=com.microsoft.azure:azure-mgmt-network:0.9.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib >> $LOG_DIR_FILE 2>>&1
+mvn dependency:copy -Dartifact=com.microsoft.azure:azure-mgmt-utility:0.9.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib >> $LOG_DIR_FILE 2>>&1
+mvn dependency:copy -Dartifact=com.microsoft.azure:azure-core:0.9.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib >> $LOG_DIR_FILE 2>>&1
+mvn dependency:copy -Dartifact=org.apache.httpcomponents:httpclient:4.5.1 -DoutputDirectory=/var/lib/hazelcast-$1/lib >> $LOG_DIR_FILE 2>>&1
+mvn dependency:copy -Dartifact=com.microsoft.azure:adal4j:1.1.2 -DoutputDirectory=/var/lib/hazelcast-$1/lib >> $LOG_DIR_FILE 2>>&1
+mvn dependency:copy -Dartifact=com.nimbusds:oauth2-oidc-sdk:4.5 -DoutputDirectory=/var/lib/hazelcast-$1/lib >> $LOG_DIR_FILE 2>>&1
+mvn dependency:copy -Dartifact=com.google.code.gson:gson:2.2.4 -DoutputDirectory=/var/lib/hazelcast-$1/lib >> $LOG_DIR_FILE 2>>&1
 
 log "Changing directory to bin to run the start.sh"
 cd /var/lib/hazelcast-$1/bin/
